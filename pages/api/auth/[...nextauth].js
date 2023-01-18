84d688d02179f1f4f407e650e7984598306529ce
steeper <portland.steeper@gmail.com>
2023-01-18 17:13:29 -0500

    didn't do much

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/spotify"
+import SpotifyProvider from "next-auth/providers/spotify"
+
 
 
 export default NextAuth = {
