104b12b40a8b39d9faf7cb9e2de9109325bb5fea
steeper <portland.steeper@gmail.com>
2023-01-19 01:17:28 -0500

    i connected to spotify's api. hooray.

--- pages/login.js
+++ pages/login.js
@@ -5,12 +5,18 @@ function Login({ providers }) {
 
 
   return (
-    <div>
+    <div className='flex flex-col items-center bg-black min-h-screen w-full justify-center'>
       <img className='w-52 mb-5' src='https:links.papareact.com/9xl' alt='' />
 
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
-          <button>Login with {provider.name}</button>
+          <button
+            className='bg-[#18D860] text-white p-5 rounded-full'
+            onClick={() => signIn(provider.id, { callbackUrl: '/'}
+            )}
+          >
+            Login with {provider.name}
+          </button>
         </div>
       ))}
     </div>
@@ -23,7 +29,6 @@ export default Login
 
 export async function getServerSideProps() {
   const providers = await getProviders()
-  console.log('Hello, Provider', providers[0])
 
   return {
     props: {
