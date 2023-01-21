0f546e2ea42079a26f9c101451735839b859a6cf
steeper <portland.steeper@gmail.com>
2023-01-20 20:41:05 -0500

    i made a small deviation from his code, just omitted the word flex on Center:8, but it works better without it

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,18 @@
 import Head from 'next/head'
 // import Image from 'next/image'
 import Sidebar from '../components/Sidebar'
+import Center from '../components/Center'
 
 
 
 export default function Home() {
 
   return (
-    <div className="">
+    <div className="bg-black h-screen overflow-hidden">
 
-      <main className='bg-black h-screen overflow-hidden'>
+      <main className='flex'>
         <Sidebar />
-        {/* Center */}
+        <Center />
       </main>
 
       <div>{/* Player */}</div>
