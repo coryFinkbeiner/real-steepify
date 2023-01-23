79c0980a3eb578e6c51a46d2b20e4418178f661c
steeper <portland.steeper@gmail.com>
2023-01-22 23:12:24 -0500

    fixed the on-load playlist image bug by adding a getServerSideProps to index.js

--- components/Center.js
+++ components/Center.js
@@ -17,12 +17,16 @@ const colors = [
    'from-purple-500'
 ];
 
+
+
 function Center() {
   const { data: session } = useSession();
   const spotifyApi = useSpotify();
   const [color, setColor] = useState(null);
   const playlistId = useRecoilValue(playlistIdState);
   const [playlist, setPlaylist] = useRecoilState(playlistState);
+
+  console.log('checking id', playlistId)
 
   useEffect(() => {
     setColor(shuffle(colors).pop());
@@ -37,12 +41,12 @@ function Center() {
       .catch((err) => console.log('ERROR', err));
   }, [spotifyApi, playlistId]);
 
-  console.log(playlist)
+  console.log('checking playlist', playlist)
 
   return (
     <div className='flex-grow'>
       <header className='absolute top-5 right-8'>
-        <div className='flex items-center bg-red-300 space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'>
+        <div className='flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white'>
           <img
             className='rounded-full w-10 h-10'
             src={session?.user.image}
@@ -52,7 +56,7 @@ function Center() {
           <ChevronDownIcon className='h-5 w-5' />
         </div>
       </header>
-      <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white padding-8`}>
+      <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}>
         <img
           className='h-44 w-44 shadow-2xl'
           src={playlist?.images?.[0]?.url}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-// import type { NextPage } from 'next'
-import Head from 'next/head'
-// import Image from 'next/image'
-import Sidebar from '../components/Sidebar'
-import Center from '../components/Center'
+import Head from 'next/head';
+import Sidebar from '../components/Sidebar';
+import Center from '../components/Center';
+import { getSession } from 'next-auth/react';
 
 
 
@@ -21,3 +20,12 @@ export default function Home() {
   )
 }
 
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session
+    }
+  };
+}
