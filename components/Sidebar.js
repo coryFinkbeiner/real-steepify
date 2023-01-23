f290c8d0448183ad68d8c8e244e979a682024766
steeper <portland.steeper@gmail.com>
2023-01-22 19:04:22 -0500

    playlists are console logging, about to render them

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,5 +1,3 @@
-// import { signOut, useSession } from 'next-auth/react'
-
 import {
   HomeIcon,
   SearchIcon,
@@ -7,13 +5,27 @@ import {
   PlusCircleIcon,
   HeartIcon,
   RssIcon
-} from '@heroicons/react/outline'
-import { signOut, useSession } from "next-auth/react"
+} from '@heroicons/react/outline';
+import { signOut, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import useSpotify from "../hooks/useSpotify";
 
 
 function Sidebar() {
-  const { data: session, status } = useSession()
-  console.log(session)
+  const spotifyApi = useSpotify();
+  const { data: session, status } = useSession();
+  const [playlists, setPlaylists] = useState([]);
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data) => {
+        setPlaylists(data.body.items)
+      })
+    }
+  }, [session, spotifyApi]);
+
+  console.log(playlists)
+
 
   return (
     <div className='text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen'>
