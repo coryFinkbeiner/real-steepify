aa038f7387bf4113ffe243c8f8306d193ef043e3
steeper <portland.steeper@gmail.com>
2023-01-23 00:38:48 -0500

    Song UI components are looking very good, about to shift to playing the Songs, and Player

--- components/Center.js
+++ components/Center.js
@@ -1,4 +1,4 @@
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import { ChevronDownIcon } from '@heroicons/react/outline';
 import { useEffect, useState } from 'react';
 import { shuffle } from 'lodash';
@@ -42,7 +42,10 @@ function Center() {
   return (
     <div className='flex-grow h-screen overflow-y-scroll scrollbar-hide'>
       <header className='absolute top-5 right-8'>
-        <div className='flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white'>
+        <div
+          className='flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 text-white'
+          onClick={signOut}
+        >
           <img
             className='rounded-full w-10 h-10'
             src={session?.user.image}
--- components/Song.js
+++ components/Song.js
@@ -5,7 +5,7 @@ function Song({ order, track }) {
   const spotifyApi = useSpotify();
 
   return (
-    <div className='grid grid-cols-2 text-gray-500'>
+    <div className='grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer'>
       <div className='flex items-center space-x-4'>
         <p>{order + 1}</p>
         <img
