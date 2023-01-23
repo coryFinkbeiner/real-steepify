13c35e63ec4c50cc2912e1745d4ad9648f46f5aa
steeper <portland.steeper@gmail.com>
2023-01-23 01:44:23 -0500

    beginning the Player component build

---
+++ components/Player.js
@@ -0,0 +1,12 @@
+
+
+function Player() {
+  return (
+    <div>
+      <h1>Player</h1>
+    </div>
+  )
+}
+
+
+export default Player
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Sidebar from '../components/Sidebar';
 import Center from '../components/Center';
+import Player from '../components/Player';
 import { getSession } from 'next-auth/react';
 
 
@@ -15,7 +16,9 @@ export default function Home() {
         <Center />
       </main>
 
-      <div>{/* Player */}</div>
+      <div className='sticky bottom-0'>
+        <Player />
+      </div>
     </div>
   )
 }
