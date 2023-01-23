a57c0b87ef1c32a89cab409ff7856b2370430519
steeper <portland.steeper@gmail.com>
2023-01-23 00:55:38 -0500

    i'm able to play songs through my spotify now

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -1,11 +1,11 @@
-import { atom } from 'recoil'
+import { atom } from 'recoil';
 
 export const playlistState = atom({
   key: 'playlistState',
   default: null
-})
+});
 
 export const playlistIdState = atom({
   key: 'playlistIdState',
   default: '0a1T2iBVme40M4W3dWY1Kx'
-})
+});
