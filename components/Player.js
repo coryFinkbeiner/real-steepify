0d81d9f51b8807f8034977f991b4dc8bf8cc1a57
steeper <portland.steeper@gmail.com>
2023-01-23 19:47:48 -0500

    about to add globals.css, but mine is built-in and he is adding his. so idk if it will be compatible.

--- components/Player.js
+++ components/Player.js
@@ -1,3 +1,4 @@
+import { SwitchHorizontalIcon } from '@heroicons/react/outline';
 import { useSession } from "next-auth/react";
 import { useState, useEffect } from "react";
 import { useRecoilState } from 'recoil';
@@ -36,9 +37,9 @@ function Player() {
   }, [currentTrackIdState, spotifyApi, session]);
 
   return (
-    <div className='h-24 bg-gradient-to-b from-black to-gray-900 text-white'>
+    <div className='h-24 bg-gradient-to-b from-black to-gray-900 text-white grid grid-cols-3 text-xs md:text-base px-2 md:px-8'>
       {/* Left */}
-      <div>
+      <div className='flex items-center space-x-4'>
         <img
           className='hidden md:inline h-10 w-10'
           src={songInfo?.album.images?.[0]?.url}
@@ -50,6 +51,10 @@ function Player() {
         </div>
       </div>
 
+      {/* Center */}
+      <div>
+        <SwitchHorizontalIcon className='w-5 h-5' />
+      </div>
     </div>
   );
 }
