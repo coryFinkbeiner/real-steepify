156bb2758af310ea13219ce0379b9a51f8dee565
steeper <portland.steeper@gmail.com>
2023-01-27 21:05:43 -0500

    search bar CSS is built

--- components/ThreadCenter.js
+++ components/ThreadCenter.js
@@ -1,12 +1,28 @@
-
+import { SearchIcon, XIcon } from '@heroicons/react/outline';
 
 function ThreadCenter() {
 
-
   return (
 
-    <div className='flex-grow h-screen overflow-y-scroll scrollbar-hide border border-white text-white '>
-      thread center
+    <div className='flex-grow h-screen overflow-y-scroll scrollbar-hide border border-white text-white'>
+      <div className=''>
+
+          <form className='bg-white text-gray-500 flex flex-row rounded-full m-3 p-1 w-2/5'>
+              <button className='flex items-center space-x-2 hover:text-black m-1'>
+                <SearchIcon className='w-5 h-5'/>
+              </button>
+
+            <input type='text' placeholder='Search Albums Only' className='w-full focus:outline-none  p-1'>
+
+            </input>
+
+
+            <button className='flex items-center space-x-2 hover:text-black m-1'>
+                <XIcon className='w-5 h-5'/>
+            </button>
+
+          </form>
+      </div>
     </div>
   );
 }
