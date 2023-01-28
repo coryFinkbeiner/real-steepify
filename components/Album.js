350c0a93cf910b6c141b74f2e020a1ead67e791c
steeper <portland.steeper@gmail.com>
2023-01-28 00:48:54 -0500

    spotify API connection started woking again randomly. albums are sized to match the monitor view of spotify client. I need to implement infinite scroll. but now i will design Album to match spotify's.

--- components/Album.js
+++ components/Album.js
@@ -5,9 +5,9 @@ function Album() {
 
   return (
 
-    <div className=''>
-
+    <div className='bg-gray-500 space-y-1 border border-white rounded p-2 h-[220px]'>
       Album
+
     </div>
   );
 }
--- components/Albums.js
+++ components/Albums.js
@@ -2,11 +2,11 @@ import Album from './Album'
 
 function Albums() {
 
-  const searchedAlbums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+  const searchedAlbums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 2, 2, 2, 2, 2, 2, 2, 2, 3, 4, 5, 4, 3, 2, 7, 4, 4]
 
   return (
 
-    <div className='h-full bg-red-500 grid grid-cols-4'>
+    <div className='h-screen bg-red-500 grid grid-cols-8 m-2 gap-3.5 p-3.5'>
 
       {searchedAlbums.map((album, i) => (
         <Album />
