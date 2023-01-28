e067a95385954eb145de3a292448c8bd86c36c6b
steeper <portland.steeper@gmail.com>
2023-01-27 21:56:12 -0500

    about to render the search results. trying to figure out the grid-col* situation.

---
+++ components/Albums.js
@@ -0,0 +1,17 @@
+
+
+function Albums() {
+
+  const searchedAlbums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+
+  return (
+
+    <div className='h-full bg-red-500'>
+
+      Albums
+
+    </div>
+  );
+}
+
+export default Albums;
