7b67bd951810958f9821258229688028a5d9a722
steeper <portland.steeper@gmail.com>
2023-01-28 19:11:02 -0500

    having a lot of issues with spotify connection. still working on the UI for album search rendering.

--- components/Album.js
+++ components/Album.js
@@ -5,8 +5,18 @@ function Album() {
 
   return (
 
-    <div className='bg-gray-500 space-y-1 border border-white rounded p-2 h-[220px]'>
-      Album
+    <div className='bg-gray-500 space-y-1 border border-white rounded p-2 h-[228px]'>
+      <img
+        className='w-full h-2/3 rounded p-1'
+        src='https://media.pitchfork.com/photos/5929b2fe9d034d5c69bf4c59/1:1/w_320,c_limit/7055fb4d.jpg'
+        alt=''
+      >
+        {/* Thread/X icon */}
+
+      </img>
+      <p className='truncate font-semibold'>
+        Album Name slkd lkjd lkjs  slk jlkasj
+      </p>
 
     </div>
   );
