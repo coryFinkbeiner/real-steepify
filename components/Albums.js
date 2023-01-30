5ee11de9da6f3eebd08dd196151d71315d5c15bf
steeper <portland.steeper@gmail.com>
2023-01-30 15:36:05 -0500

    successfully pulling the searchResults atom into Albums.js

--- components/Albums.js
+++ components/Albums.js
@@ -1,15 +1,22 @@
 import Album from './Album'
+import { useRecoilValue } from 'recoil';
+import { searchResultsState } from "../atoms/albumsAtom";
+
 
 function Albums() {
 
-  const searchedAlbums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 2, 2, 2, 2, 2, 2, 2, 2, 3, 4, 5, 4, 3, 2, 7, 4, 4]
+  const searchedAlbums = useRecoilValue(searchResultsState)
+  console.log({searchedAlbums})
 
   return (
 
     <div className='h-screen bg-red-500 grid lg:grid-cols-8 md:grid-cols-4 m-2 gap-3.5 p-3.5'>
 
       {searchedAlbums.map((album, i) => (
-        <Album  key={i}/>
+        <Album
+          key={i}
+          
+        />
       ))}
 
 
