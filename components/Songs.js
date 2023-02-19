2116ded1326ae9c7a29dd8e4a5d85ab7912bdf0a
steeper <portland.steeper@gmail.com>
2023-02-19 17:14:48 -0500

    had to create a sloppy workaround to bringing the album track info to this context, created album1,2,3 hooks.

--- components/Songs.js
+++ components/Songs.js
@@ -6,6 +6,8 @@ import Song from './Song'
 function Songs() {
   const playlist = useRecoilValue(playlistState)
 
+  // console.log({playlist})
+
   return (
     <div className='px-8 flex flex-col space-y-1 pb-28 text-white'>
       {playlist?.tracks.items.map((track, i) => (
--- components/ThreadedSongs.js
+++ components/ThreadedSongs.js
@@ -10,88 +10,32 @@ function ThreadedSongs() {
   const spotifyApi = useSpotify();
   const [trackList, setTrackList] = useState([])
 
+  const [album1, setAlbum1] = useState([])
+  const [album2, setAlbum2] = useState([])
+  const [album3, setAlbum3] = useState([])
 
-  // I GUESS I SHOULD THREAD THEM FIRST...
 
 
-
-  // I THINK YOU WILL NOT KEEP THIS
   useEffect(() => {
+
     spotifyApi.getAlbumTracks(bank[0].id)
-      .then(data => {
-        setTrackList([data.body.items])
-        spotifyApi.getAlbumTracks(bank[1].id)
-        .then(data => {
+      .then(data => setAlbum1(data.body.items))
 
-          console.log('2', data.body.items)
+    spotifyApi.getAlbumTracks(bank[1].id)
+      .then(data => setAlbum2(data.body.items))
 
-          setTrackList([...trackList, data.body.items])
-          console.log({trackList})
+    spotifyApi.getAlbumTracks(bank[2].id)
+      .then(data => setAlbum3(data.body.items))
 
-
-        //   spotifyApi.getAlbumTracks(bank[2].id)
-        //   .then(data => {
-
-
-        //     setTrackList([...trackList, ...data.body.items])
-
-
-        //     var thread = trackList.slice()
-        //     console.log({thread})
-
-        //   })
-
-        })
-
-      })
-
-
-
-
-
-
-
-  }, [bank, spotifyApi]);
-
-
-
-
-  //   useEffect
-
-  //     getAlbumTracks: function(albumId, options, callback) {
-  //     }.then  setThreadedTracks
-
-
-  //    }, [bank]
-
-  //     from useSongInfo hook
-  //
-  // useEffect(() => {
-  //   const fetchSongInfo = async () => {
-  //     if (currentIdTrack) {
-  //       const trackInfo = await fetch(
-  //         `https://api.spotify.com/v1/tracks/${currentIdTrack}`,
-  //         {
-  //           headers: {
-  //             Authorization: `Bearer ${spotifyApi.getAccessToken()}`
-  //           }
-  //         }
-  //       ).then((res) => res.json());
-
-  //       setSongInfo(trackInfo);
-  //     }
-  //   }
-
+  }, [spotifyApi, bank])
 
 
   return (
     <div className='px-8 flex flex-col space-y-1 pb-28 text-white'>
-      {/* {threadedTracks?.tracks.items.map((track, i) => (
-        <Song key={track.track.id} track={track} order={i}/>
-      ))} */}
-      {trackList[25]?.href}
     </div>
   )
+
 }
 
 export default ThreadedSongs;
+
